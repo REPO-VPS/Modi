a824e68c01a5806263b0ee23beac7018b36945b1
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-02-20 12:42:32 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -21,7 +21,7 @@ P='\e[0;35m'
 B='\033[0;36m'
 G='\e[0;32m'
 N='\e[0m'
-export Server_URL="raw.githubusercontent.com/annelyah23/xyz/main"
+export Server_URL="raw.githubusercontent.com/Jesanne87/Modi/main"
 
 clear
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
@@ -40,77 +40,11 @@ tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-cek=$( curl -sS https://raw.githubusercontent.com/annelyah23/IP/main/access | awk '{print $2}'  | grep $MYIP )
-Name=$(curl -sS https://raw.githubusercontent.com/annelyah23/IP/main/access | grep $MYIP | awk '{print $4}')
-if [[ $cek = $MYIP ]]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo ""
-echo -e "Your IP is ${red}NOT REGISTER${NC} @ ${red}EXPIRED${NC}"
-echo ""
-echo -e "Please Contact ${green}Admin${NC}"
-echo -e "Telegram : t.me/JsPhantom"
-exit 0
-fi
-clear
-
-BURIQ() {
-    curl -sS https://raw.githubusercontent.com/annelyah23/IP/main/access >/root/tmp
-    data=($(cat /root/tmp | grep -E "^### " | awk '{print $4}'))
-    for user in "${data[@]}"; do
-        exp=($(grep -E "^### $user" "/root/tmp" | awk '{print $3}'))
-        d1=($(date -d "$exp" +%s))
-        d2=($(date -d "$biji" +%s))
-        exp2=$(((d1 - d2) / 86400))
-        if [[ "$exp2" -le "0" ]]; then
-            echo $user >/etc/.$user.ini
-        else
-            rm -f /etc/.$user.ini >/dev/null 2>&1
-        fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-Name=$(curl -sS https://raw.githubusercontent.com/annelyah23/IP/main/access | grep $MYIP | awk '{print $4}')
-echo $Name >/usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman() {
-    if [[ -f "/etc/.$Name.ini" ]]; then
-        CekTwo=$(cat /etc/.$Name.ini)
-        if [[ "$CekOne" = "$CekTwo" ]]; then
-            res="Expired"
-        fi
-    else
-        res="Permission Accepted..."
-    fi
-}
-
-PERMISSION() {
-    MYIP=$(wget -qO- ipv4.icanhazip.com);
-    IZIN=$(curl -sS https://raw.githubusercontent.com/annelyah23/IP/main/access | awk '{print $3}' | grep $MYIP)
-    if [[ "$MYIP" = "$IZIN" ]]; then
-        Bloman
-    else
-        res="Permission Denied!"
-    fi
-    BURIQ
-}
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-
-if [ "$res" = "Expired" ]; then
-Exp="\e[36mExpired\033[0m"
-rm -f /home/needupdate > /dev/null 2>&1
-else
-Exp=$(curl -sS https://raw.githubusercontent.com/annelyah23/IP/main/access | grep $MYIP | awk '{print $3}')
-fi
 
 # // Export Color & Information
 export RED='\033[0;31m'
@@ -151,7 +85,7 @@ clear
 echo -e "[ ${GREEN}INFO${NC} ] Check for Script updates . . ."
 sleep 2
 cd
-wget -q -O /root/update-v2.sh "https://raw.githubusercontent.com/annelyah23/xyz/main/update-v2.sh" && chmod +x update-v2.sh && ./update-v2.sh
+wget -q -O /root/update-v2.sh "https://raw.githubusercontent.com/Jesanne87/Modi/main/update-v2.sh" && chmod +x update-v2.sh && ./update-v2.sh
 sleep 2
 rm -f /root/update-v2.sh
 rm -f /home/ver
