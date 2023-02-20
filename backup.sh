3e536e1fa5ae9bfeaa5509436dacee2da71adc31
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-02-20 12:49:15 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -12,51 +12,109 @@ purple='\e[0;35m'
 orange='\e[0;33m'
 NC='\e[0m'
 clear
-IP=$(wget -qO- icanhazip.com);
 date=$(date +"%Y-%m-%d")
-clear
-echo " VPS Data Backup By JsPhantom "
-sleep 1
-echo ""
-echo -e "[ ${green}INFO${NC} ] Please Insert Password To Secure Backup Data ."
-echo ""
-read -rp "Enter password : " -e InputPass
-clear
+itoken=$(curl -sS https://raw.githubusercontent.com/SSHSEDANG4/update/main/asu)
+
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1│${NC} ${COLBG1}             • BACKUP PANEL MENU •             ${NC} $COLOR1│$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1│${NC}  [INFO] Create password for database"
+read -rp "   [INFO] Enter password : " -e InputPass
 sleep 1
 if [[ -z $InputPass ]]; then
 exit 0
 fi
-echo -e "[ ${green}INFO${NC} ] Processing . . . "
+echo -e "$COLOR1│${NC}  [INFO] Processing... "
 mkdir -p /root/backup
 sleep 1
-clear
-echo " Please Wait VPS Data Backup In Progress . . . "
+
 cp -r /root/.acme.sh /root/backup/ &> /dev/null
-cp -r /var/lib/premium-script/ /root/backup/premium-script
-cp -r /usr/local/etc/xray /root/backup/xray
-cp -r /usr/local/etc/xray/*.json /root/backup/ >/dev/null 2>&1
-cp -r /home/vps/public_html /root/backup/public_html
+cp /etc/passwd /root/backup/ &> /dev/null
+cp /etc/group /root/backup/ &> /dev/null
+cp /etc/shadow /root/backup/ &> /dev/null
+cp /etc/gshadow /root/backup/ &> /dev/null
+cp /etc/ppp/chap-secrets /root/backup/chap-secrets &> /dev/null
+cp /etc/ipsec.d/passwd /root/backup/passwd1 &> /dev/null
+cp -r /var/lib/ssnvpn-pro/ /root/backup/ssnvpn-pro &> /dev/null
+cp -r /etc/xray /root/backup/xray &> /dev/null
+cp -r /home/vps/public_html /root/backup/public_html &> /dev/null
 cp -r /etc/cron.d /root/backup/cron.d &> /dev/null
-cp -r /etc/crontab /root/backup/crontab &> /dev/null
+cp /etc/crontab /root/backup/crontab &> /dev/null
 cd /root
-zip -rP $InputPass $IP-$date.zip backup > /dev/null 2>&1
-rclone copy /root/$IP-$date.zip dr:backup/
-url=$(rclone link dr:backup/$IP-$date.zip)
-id=(`echo $url | grep '^https' | cut -d'=' -f2`)
-link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-clear
-echo -e "\033[1;37mVPS Data Backup By JsPhantom\033[0m
-\033[1;37mTelegram : https://t.me/JsPhantom / @JsPhantom\033[0m"
-echo ""
-echo "Please Copy Link Below & Save In Notepad"
-echo ""
-echo -e "Your VPS IP ( \033[1;37m$IP\033[0m )"
-echo ""
-echo -e "Your VPS Data Backup Password : \033[1;37m$InputPass\033[0m"
-echo ""
-echo -e "\033[1;37m$link\033[0m"
-echo ""
-echo "If you want to restore data, please enter the link above"
-rm -rf /root/backup
-rm -r /root/$IP-$date.zip
-echo ""
+zip -rP $InputPass $NameUser.zip backup > /dev/null 2>&1
+
+##############++++++++++++++++++++++++#############
+LLatest=`date`
+Get_Data () {
+git clone https://github.com/SSHSEDANG4/backupuserssn.git /root/user-backup/ &> /dev/null
+}
+
+Mkdir_Data () {
+mkdir -p /root/user-backup/$NameUser
+}
+
+Input_Data_Append () {
+if [ ! -f "/root/user-backup/$NameUser/$NameUser-last-backup" ]; then
+touch /root/user-backup/$NameUser/$NameUser-last-backup
+fi
+echo -e "User         : $NameUser
+last-backup : $LLatest
+" >> /root/user-backup/$NameUser/$NameUser-last-backup
+mv /root/$NameUser.zip /root/user-backup/$NameUser/
+}
+
+Save_And_Exit () {
+    DATE=$(date +'%d %B %Y')
+    cd /root/user-backup
+    git config --global user.email "sshsedang@gmail.com" &> /dev/null
+    git config --global user.name "SSHSEDANG4" &> /dev/null
+    rm -rf .git &> /dev/null
+    git init &> /dev/null
+    git add . &> /dev/null
+    git commit -m backup &> /dev/null
+    git branch -M main &> /dev/null
+    git remote add origin https://github.com/SSHSEDANG4/backupuserssn
+    git push -f https://$itoken@github.com/SSHSEDANG4/backupuserssn.git &> /dev/null
+}
+
+if [ ! -d "/root/user-backup/" ]; then
+sleep 1
+echo -e "$COLOR1│${NC}  [INFO] Getting database... "
+Get_Data
+Mkdir_Data
+sleep 1
+echo -e "$COLOR1│${NC}  [INFO] Getting info server... "
+Input_Data_Append
+sleep 1
+echo -e "$COLOR1│${NC}  [INFO] Processing updating server...... "
+Save_And_Exit
+fi
+link="https://raw.githubusercontent.com/SSHSEDANG4/backupuserssn/main/$NameUser/$NameUser.zip"
+sleep 1
+echo -e "$COLOR1│${NC}  [INFO] Backup done "
+sleep 1
+echo
+sleep 1
+echo -e "$COLOR1│${NC}  [INFO] Generete Link Backup "
+echo -e "$COLOR1│${NC}"
+sleep 2
+echo -e "$COLOR1│${NC}  The following is a link to your vps data backup file.
+$COLOR1│${NC}  Your VPS IP $IP
+$COLOR1│${NC}
+$COLOR1│${NC}  $link
+$COLOR1│${NC}  save the link pliss!
+$COLOR1│${NC}
+$COLOR1│${NC}  If you want to restore data, please enter the link above.
+$COLOR1│${NC}  Thank You For Using Our Services"
+cd
+rm -rf /root/backup &> /dev/null
+rm -rf /root/user-backup &> /dev/null
+rm -f /root/$NameUser.zip &> /dev/null
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
+echo -e "$COLOR1│${NC}            •   JsPhantom-VPN   •                "
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+echo
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
