794278234aea4824483a9323841430e4b5a84103
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-02-20 13:32:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@
 # Auther  : JsPhantom
 # (C) Copyright 2023
 # =========================================
-clear
 DEFBOLD='\e[39;1m'
 RB='\e[31;1m'
 GB='\e[32;1m'
@@ -25,13 +24,11 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 clear
-
-red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 tyblue='\e[1;36m'
 purple='\e[0;35m'
-NC='\e[0m'
+
 purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
 tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
